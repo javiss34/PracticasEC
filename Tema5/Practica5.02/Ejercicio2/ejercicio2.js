f90564f2942539c46b66c5eeaff13c7c72da4dd8
javisr34 <javsansol@alu.edu.gva.es>
2025-10-29 17:33:45 +0100

    Práctica5.02 terminada

--- Tema5/Practica5.02/Ejercicio2/ejercicio2.js
+++ Tema5/Practica5.02/Ejercicio2/ejercicio2.js
@@ -1,18 +1,22 @@
 "use strict";
 
 window.onload = () => {
-    const pestañas = document.querySelectorAll(".pestañas p")
-    const infoPestañas = document.querySelectorAll(".info_pestañas p")
-    console.log(pestañas)
+    const pestañas = document.querySelectorAll(".pestañas p")//Obtengo un nodeList de los párrafos dentro del div .pestañas
+    const infoPestañas = document.querySelectorAll(".info_pestañas p")//Obtengo un nodeList de la información dentro del div .info_pestañas
 
-    pestañas.forEach((pestaña) => {
+    pestañas.forEach((pestaña,i) => {
         pestaña.addEventListener(
             "click",
             () => {
-                infoPestañas.forEach((info) => {
-                    info.classList.toggle("mostrar");
-                    console.log(info);
-                })
+                //Pasamos por cada elemnto del nodelist infopestañas y quito la clase mostrar.
+                /* infoPestañas.forEach((info) => {
+                    info.classList.remove("mostrar");
+                }) */
+
+                //Añado al elemento pestaña la clase mostrar que está en el css.
+                infoPestañas[i].classList.toggle("mostrar");
+
+
             }
         )
     })
