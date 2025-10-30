2af42a3c1a53be2b9ca494858768f2a31e74f91b
javisr34 <javsansol@alu.edu.gva.es>
2025-10-30 19:04:00 +0100

    Ejercicio1Practica5.02 hecho bien

--- Tema5/Practica5.02/Ejercicio1/ejercicio1.js
+++ Tema5/Practica5.02/Ejercicio1/ejercicio1.js
@@ -1,8 +1,7 @@
 "use strict";
 
 window.onload = () => {
-  let elemento = document.querySelectorAll("div div"); //Aquí selecciono los 6 div que hay dentro del div .contenedor_ejercicio
-  console.log(elemento);
+  let elemento = document.querySelectorAll("div div");//
   //Función para comprobar si un número es par
   const esPar = (num) => {
     if (num % 2 === 0) {
@@ -14,14 +13,13 @@ window.onload = () => {
 
   //recorremos el NodeList
   elemento.forEach((div, i) => {
-    if (esPar(i)) {
-      //Si el contador es par(0, 2 o 4) se hace el evento
-      div.addEventListener("click", () => {
-        let siguiente = elemento[i + 1];
-        if (siguiente) {//Comprobamos que siguiente existe
-          siguiente.classList.toggle("mostrar"); //Aplicamos el estilo del css .mostrar para el siguiente elemento del contador
+      div.addEventListener("click", (evento) => {
+        if(esPar(i)){
+          if(evento.target===div){
+            let contenido = div.nextElementSibling;
+            contenido.classList.toggle("mostrar");
+          }
         }
       });
-    }
   });
 };
