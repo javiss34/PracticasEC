98bd396ab79f6ca8279d1f9797754f147b0665b4
javisr34 <javiersanchezsolera.alu@iespacomolla.es>
2025-11-07 13:46:38 +0100

    Puzle casi acabado

--- Tema5/Practica5.04/biblioteca.js
+++ Tema5/Practica5.04/biblioteca.js
@@ -1,37 +1,29 @@
 "use strict";
 
-let contenedorArrastrable = document.getElementById("zonaArrastrable");
-//Función para ordenar las imágenes aleatoriamente y asignarles los atributos src,class e id.
-export const ordenarArrayImagenes = (imagenes,div) => {
-      let todosLosIndices = [];
-    
-      for (let i = 0; i < imagenes.length; i++) {
-        let indiceAleatorio;
-    
-        do {
-          indiceAleatorio = Math.floor(Math.random() * imagenes.length);
-        } while (todosLosIndices.includes(indiceAleatorio));
-    
-        todosLosIndices = [...todosLosIndices, indiceAleatorio];
-        let elementoImg = document.createElement("img");
-        elementoImg.setAttribute("src", imagenes[indiceAleatorio]);
-        elementoImg.setAttribute("class", "arrastrable");
-        elementoImg.setAttribute("id", i+1);
-        div.appendChild(elementoImg);
-      }
-}
-
-//Función para crear 9 divs con el className "soltable"
-export const crearDiv3x3 = (div) => {
-
-  for (let i = 0; i < 9; i++) {
-    let nuevoDiv = document.createElement("div");
-    nuevoDiv.className = "soltable";
-    div.appendChild(nuevoDiv);
+export const desordenarIndices = (array,contenedorPadre) => {
+  let todosLosIndices = [];
+  for (let i = 0; i < array.length; i++) {
+    let numeroAleatorio;
+    do {
+      numeroAleatorio = Math.floor(Math.random() * array.length);
+    } while (todosLosIndices.includes(numeroAleatorio));
+    todosLosIndices = [...todosLosIndices, numeroAleatorio];
+    contenedorPadre.appendChild(array[numeroAleatorio]);
   }
 }
 
-//Función para reiniciar la partida
-export const botonReinicio = () => {
-  
+export const comprobarPuzle = (panelJuego,divs,imagenOriginal) => {
+    let piezasColocadas = 0;
+    for (let i = 0; i < divs.length; i++) {
+        if(divs[i].children.length>0){
+            piezasColocadas++;
+        }
+    }
+    if(piezasColocadas===imagenOriginal.length){
+        for (let i = 0; i < array.length; i++) {
+            
+        }
+    }
+
 }
+
--- Tema5/Practica5.04/rompecabezas.js
+++ Tema5/Practica5.04/rompecabezas.js
@@ -1,104 +1,85 @@
 "use strict";
-import { ordenarArrayImagenes,crearDiv3x3 } from "./biblioteca.js";
+import { desordenarIndices } from "./biblioteca.js";
 
 window.onload = () => {
-  let contenedorArrastrable = document.getElementById("zonaArrastrable");
-  let imagenes = [
-    "img/fila-1-columna-1.jpg",
-    "img/fila-1-columna-2.jpg",
-    "img/fila-1-columna-3.jpg",
-    "img/fila-2-columna-1.jpg",
-    "img/fila-2-columna-2.jpg",
-    "img/fila-2-columna-3.jpg",
-    "img/fila-3-columna-1.jpg",
-    "img/fila-3-columna-2.jpg",
-    "img/fila-3-columna-3.jpg",
-  ];
-  
-  //Primer div:
-  let primerDiv = document.createElement("div");
-  primerDiv.className = "div_superior";
-  contenedorArrastrable.appendChild(primerDiv);
-  //Desordenamos las imágenes de forma aleatoria con esta función que está en la biblioteca
-  ordenarArrayImagenes(imagenes,primerDiv);
-  
+  let contenedorImagenes = document.getElementById("piezas");
+  let imagenes = contenedorImagenes.children;
+  let panelJuego = document.getElementById("panel_juego");
+  let divsPanelJuego = panelJuego.children;
+  let todasLasImagenes = document.querySelectorAll("div img");
+  console.log(todasLasImagenes)
 
-  //Segundo div: Panel de juego
-  let segundoDiv = document.createElement("div");
-  segundoDiv.className = "div_intermedio";
-  contenedorArrastrable.appendChild(segundoDiv);
-  //Creamos los 9 divs con esta función que está en la biblioteca
-  crearDiv3x3(segundoDiv);
+  //Desordenar imagenes
+  desordenarIndices(imagenes, contenedorImagenes);
 
-  //Tercer div: Botón de reinicio
-  let botonReinicio = document.getElementById("botonReinicio");
-  console.log(botonReinicio)
-  
-  
-
-
-  const elementosArrastrables = document.getElementsByClassName("arrastrable");
-  //Le asignamos a cada imágen el draggable para que se puedan arrastrar
+  //Ponemos el draggable a cada elemento arrastrable
+  let zonaArrastrable = document.getElementById("zona_arrastrable");
+  let elementosArrastrables = document.getElementsByClassName("arrastrable");
   for (let i = 0; i < elementosArrastrables.length; i++) {
-    elementosArrastrables[i].setAttribute("draggable",true);
+    elementosArrastrables[i].setAttribute("draggable", true);
   }
 
-  let zonaArrastrable = document.getElementById("zonaArrastrable");
-  //En primer luegar le asignamos un id y un nombre al comenzar a arrastrar
-  zonaArrastrable.addEventListener(
-    "dragstart",
-    (evento) => {
-      evento.dataTransfer.setData("id", evento.target.id);
-      evento.dataTransfer.setData("nombre",evento.target.localName);
+  //Establecemos id y nombre al comenzar a arrastrar
+  zonaArrastrable.addEventListener("dragstart", (evento) => {
+    evento.dataTransfer.setData("id", evento.target.id);
+    evento.dataTransfer.setData("etiqueta", evento.target.localName);
+  });
 
-      console.log(evento.dataTransfer.getData("id"));
-      console.log(evento.dataTransfer.getData("nombre"));
-    },
-    false
-  )
+  //Neecesitamos hacer el preventDefault para que al deslizar nos deje soltar
+  zonaArrastrable.addEventListener("dragover", (evento) => {
+    evento.preventDefault();
+  });
 
-  //En segundo lugar hacemos el dragover para que cuando queramos soltar la imagen en un soltable nos deje
-  zonaArrastrable.addEventListener(
-    "dragover",
-    (evento) => {
-      evento.preventDefault();
-    },
-    false
-  )
+  zonaArrastrable.addEventListener("drop", (evento) => {
+    evento.preventDefault;
+    console.log(evento);
+    if (evento.target.classList.contains("soltable")) {
+      evento.target.appendChild(
+        document.getElementById(evento.dataTransfer.getData("id"))
+      );
+    }
 
-  //En tercer lugar añadimos al soltable el elemento que estoy arrastrando
-  zonaArrastrable.addEventListener(
-    "drop",
-    (evento) => {
-      evento.preventDefault();
-      //De esta forma se pueden soltar los trozos de imagen tanto del primer div al segundo, como del segundo al primero.
-      if(evento.target.classList.contains("soltable") || evento.target.classList.contains("div_superior")){
-        evento.target.appendChild(document.getElementById(evento.dataTransfer.getData("id")))
-      }
-    },
-    false
-  )
 
-  botonReinicio.addEventListener(
-    "drop",
-    (evento) => {
-      evento.preventDefault();
-      if(evento.target.classList.contains("soltable")){
-        evento.target.appendChild(document.getElementById(evento.dataTransfer.getData("id")))
+    let piezasColocadas = 0;
+    for (let i = 0; i < divsPanelJuego.length; i++) {
+      if (divsPanelJuego[i].children.length > 0) {
+        piezasColocadas++;
       }
     }
-  )
 
+    if (piezasColocadas === 9) {
+      let puzzleCorrecto = true;
 
+      for (let i = 0; i < divsPanelJuego.length; i++) {
+        let idDiv = divsPanelJuego[i].id;
+        let idImg = divsPanelJuego[i].children[0].id;
 
+        if (idDiv !== idImg) {
+          puzzleCorrecto = false;
+          break;
+        }
+      }
 
+      let felicitaciones = document.createElement("h1");
+      if (puzzleCorrecto) {
+        felicitaciones.innerHTML = "Felicidades, puzzle completado";
+      } else {
+        felicitaciones.innerHTML = "Lo siento, puzzle mal hecho";
+      }
 
+      document.body.appendChild(felicitaciones);
+    }
+  });
 
-
-
-
-  
-
-
-
-};
+  let botonReinicio = document.getElementById("boton_reinicio");
+  botonReinicio.addEventListener("click", () => {
+    // 1. Llamamos a desordenarIndices para mover todas las piezas de vuelta a 'piezas'
+    desordenarIndices(todasLasImagenes, contenedorImagenes);
+    
+    // 2. Quitamos el mensaje de felicitación si existe
+    const mensajeAnterior = document.getElementById("mensaje-final");
+    if (mensajeAnterior) {
+        mensajeAnterior.remove();
+    }
+  });
+}; //FIN ONLOAD
