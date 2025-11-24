91a99a59e2fbace117b1d307b4c3d3b0281286f0
javisr34 <javsansol@alu.edu.gva.es>
2025-11-24 19:00:09 +0100

    Comienzo tema 6

---
+++ Tema6/Practica6.01(AE)/ejercicio1/biblioteca.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const generarNumeroEntreUnoYcien = () => {
+    return Math.floor(Math.random()*100+1);
+}
---
+++ Tema6/Practica6.01(AE)/ejercicio1/ejercicio1.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import {generarNumeroEntreUnoYcien} from './biblioteca.js'
+window.onload = () => {
+    const contenedor = document.getElementById("contenedor");
+
+    const promesa = new Promise((resolver,rechazar)=>{
+        setTimeout = (() => {
+
+        })
+    },2000)
+
+}
